9f7a101907127330e2d8f36099d7ef60abc76ea4
bowoo96 <qhdn131@naver.com>
2022-09-21 22:11:52 +0900

    [ Feat ] : Develop Record Add Feature

--- src/components/RecordItem.tsx
+++ src/components/RecordItem.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { ModalTitleDiv, RecordButton } from "./common/commonStyle";
+import { Recorditem } from "../components/modal/Modal";
 
-const RecordItem = () => {
+const RecordItem: React.FC<{ recordData: Recorditem }> = (props) => {
+  console.log(props.recordData);
   return (
-    <li>
-      <ModalTitleDiv></ModalTitleDiv>
-      <ModalTitleDiv></ModalTitleDiv>
-      <ModalTitleDiv></ModalTitleDiv>
+    <RecordItemWrap>
+      <ModalTitleDiv>{props.recordData.exerciseType}</ModalTitleDiv>
+      <ModalTitleDiv>{props.recordData.set}</ModalTitleDiv>
+      <ModalTitleDiv>{props.recordData.count}</ModalTitleDiv>
       <RecordButton>x</RecordButton>
-    </li>
+    </RecordItemWrap>
   );
 };
 
 export default RecordItem;
+
+const RecordItemWrap = styled.li`
+  display: flex;
+  margin-top: 20px;
+`;
